3e1b35a9340cb9657804599358f935b496ba6b65
mauriciogofas <mauricio@gofas.net>
2023-04-10 14:21:49 -0300

    v3.9.1

--- modules/gateways/gofasgerencianetboleto.php
+++ modules/gateways/gofasgerencianetboleto.php
@@ -6,8 +6,11 @@
  * @copyright	2016 -> 2023 Gofas Software
  * @license		https://gofas.net?p=9340
  * @support		https://gofas.net/?p=7856
- * @version		3.9.0
+ * @version		3.9.1
  */
 if((int)substr(preg_replace('/[^\da-z]/i','',phpversion()),0,2)>=(int)81){
     require __DIR__.'/gofasgerencianetboleto/index.php';
+}
+if((int)substr(preg_replace('/[^\da-z]/i','',phpversion()),0,2)<=(int)74){
+    require __DIR__.'/gofasgerencianetboleto/indexd.php';
 }
