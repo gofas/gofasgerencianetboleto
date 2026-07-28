c99188ebecac142cc72035021413bbf1d0814fc2
Zadig <mauricio@gofas.net>
2026-07-28 00:51:17 +0000

    #204: bump 3.14.1

--- modules/gateways/gofasgerencianetboleto/index.php
+++ modules/gateways/gofasgerencianetboleto/index.php
@@ -6,7 +6,7 @@
  * @copyright	2016 -> 2025 Gofas Software
  * @license		https://gofas.net?p=9340
  * @support		https://gofas.net/?p=7856
- * @version		3.14.0
+ * @version		3.14.1
  */
 use WHMCS\Application;
 use WHMCS\Database\Capsule;
